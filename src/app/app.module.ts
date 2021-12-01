e2a572b2352cad53816113fa95075c0aa48d92ef
ADITYA SANCHETI <Aditya.Sancheti@citiustech.com>
2021-12-01 14:21:49 +0530

    Angular Material, syncfusion and main layout added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { RouterModule, Routes } from '@angular/router';
+
+import { AngularMaterialModule } from './modules/angular-material.module';
+import { SyncfusionModule } from './modules/syncfusion.module';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+
+const routes: Routes = [
+  //main
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: MainLayoutComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, MainLayoutComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    AngularMaterialModule,
+    SyncfusionModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
